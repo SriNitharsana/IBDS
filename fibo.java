827e335b19afe626260c5df75d57d384beeb7132
SriNitharsana <srinitharsana27@gmail.com>
2025-09-04 14:19:59 +0530

    Update fibo.java

--- fibo.java
+++ fibo.java
@@ -1,1 +1,23 @@
 
+// fibo.java
+import java.util.Scanner;
+
+public class fibo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1, c;
+
+        System.out.println("Fibonacci Series:");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        sc.close();
+    }
+}
