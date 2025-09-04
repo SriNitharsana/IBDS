658953eb57edf55022fb23dc1633fca23402d887
SriNitharsana <srinitharsana27@gmail.com>
2025-09-04 14:20:22 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -1,1 +1,21 @@
 
+// sum.java
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter how many numbers to add: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            sum += sc.nextInt();
+        }
+
+        System.out.println("Sum = " + sum);
+        sc.close();
+    }
+}
